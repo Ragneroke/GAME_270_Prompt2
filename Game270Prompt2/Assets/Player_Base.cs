1bd8db78c610f493c0c39c66350ef39cd7e5c9da
Ragneroke <46947503+Ragneroke@users.noreply.github.com>
2021-04-19 14:25:47 -0700

    Initialize 2d platformer scene, include tileset, character controllers

---
+++ Game270Prompt2/Assets/Player_Base.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Base : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D controller;
+    private SpriteRenderer sr;
+    public float moveSpeed  = 40f;
+    float horizontalMove = 0f;
+    private int maxHealth = 100;
+    private int health;
+    // Damage cool down variables
+    private float lastHit = 0f;
+    private float hitCd = 1f;
+    [SerializeField] private bool jump = false;
+    [SerializeField] private bool isInvic = false;
+    void Start()
+    {
+        health = maxHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            
+            jump = true;
+            
+        }
+        
+    }
+
+    void FixedUpdate() 
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
