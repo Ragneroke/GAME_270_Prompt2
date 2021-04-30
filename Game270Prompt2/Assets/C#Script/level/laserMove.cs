b6b910a0b4cd8e3a820716a368b2b12f8883deec
HongkaiD <75577452+HongkaiD@users.noreply.github.com>
2021-04-30 08:54:16 -0400

    change the laser movement speeds

--- Game270Prompt2/Assets/C#Script/level/laserMove.cs
+++ Game270Prompt2/Assets/C#Script/level/laserMove.cs
@@ -6,7 +6,12 @@ public class laserMove : MonoBehaviour
 {
     [SerializeField]
     int direction = 1;
+    [SerializeField]
     float speed;
+
+    public float stay;
+    public float cd;
+
     Vector3 v;
     // Start is called before the first frame update
     void Start()
@@ -17,12 +22,17 @@ public class laserMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        speed = Random.Range(100f,120f);
+        speed = Random.Range(10f,12f);
 
         v.y += direction * Time.deltaTime * speed;
         if (transform.position.y > 35f)
         {
-            direction = -1;
+            stay -= Time.deltaTime;
+            if (stay < 0)
+            {
+                stay = cd;
+                direction = -1;
+            }
         }
         if (transform.position.y < 21f){
             direction = 1;
